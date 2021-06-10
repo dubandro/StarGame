3bd91808ae59e2cf6dea5162fcca4654ec57a762
dubandro <dubovik@internet.ru>
2021-06-10 17:09:16 +0200

    lesson5

---
+++ core/src/ru/gb/sprite/Enemy.java
@@ -0,0 +1,11 @@
+package ru.gb.sprite;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import ru.gb.base.Sprite;
+
+public class Enemy extends Sprite {
+    public Enemy(TextureRegion region) {
+        super(region);
+    }
+}
